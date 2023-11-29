1c3214d7cce37ec106181b88d8adcfe1cd3095af
lebugne <saf.kabidi@gmail.com>
2023-11-29 11:44:29 +0100

    Correcting menu

--- src/menu.cpp
+++ src/menu.cpp
@@ -8,51 +8,34 @@ Menu::Menu() {
 Menu::~Menu() {}
 
 void Menu::drawStart() {
-    // TODO: Where the textures are going to be positionated?
-    // TODO: Having enum as index for textures is more suitable
+
 
     /* Title */
-    // Texture2D titleTexture = LoadTexture("assets/menu/title.png");
-    // Vector2 title          = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.2f };
-    // float titleScale       = 0.2f;
-    // DrawTextureEx(titleTexture, { title.x - (titleTexture.width * titleScale)/2.0f, title.y - (titleTexture.height * titleScale)/2.0f }, 0.0f, titleScale, WHITE);
+    Texture2D titleTexture = LoadTexture("assets/menu/title.png");
+    Vector2 title          = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.2f };
+    float titleScale       = 0.2f;
+    DrawTextureEx(titleTexture, { title.x - (titleTexture.width * titleScale)/2.0f, title.y - (titleTexture.height * titleScale)/2.0f }, 0.0f, titleScale, WHITE);
 
     /* play */
-    // Texture2D playTexture = LoadTexture("assets/menu/play.png");
-    // Vector2 play          = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.6f };
-    // float playScale       = 0.3f;
-    // DrawTextureEx(playTexture, { play.x - (playTexture.width * playScale)/2.0f, play.y - (playTexture.height * playScale)/2.0f }, 0.0f, playScale, WHITE);
-
-    /* =============================================================================== */
-
-    /* pause */
-    Vector2 pause = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.3f };
-    DrawTextEx(GetFontDefault(), "Game Over" , pause , 20.0f , 5.0f , BLACK);
+    Texture2D playTexture = LoadTexture("assets/menu/play.png");
+    Vector2 play          = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.6f };
+    float playScale       = 0.3f;
+    DrawTextureEx(playTexture, { play.x - (playTexture.width * playScale)/2.0f, play.y - (playTexture.height * playScale)/2.0f }, 0.0f, playScale, WHITE);
 
 
-    /* retry */
-    Vector2 retry = { GetScreenWidth() * 0.45f, GetScreenHeight() * 0.6f };
-    DrawTextEx(GetFontDefault(), "Retry" , retry , 20.0f , 5.0f , BLACK);
-
-    /* play */
-    Vector2 play = { GetScreenWidth() * 0.55f, GetScreenHeight() * 0.6f };
-    DrawTextEx(GetFontDefault(), "Play" , play , 20.0f , 5.0f , BLACK);
 
 }
 
 void Menu::drawLevels() {}
 
 void Menu::drawGameOver() {
-    Texture2D gameOverTexture = LoadTexture("assets/menu/text/gameOver.png");
+
     Vector2 gameOver          = { GetScreenWidth() / 2.0f, GetScreenHeight() * 0.3f };
-    float gameOverScale       = 0.15f;
-    DrawTextureEx(gameOverTexture, { gameOver.x - (gameOverTexture.width * gameOverScale)/2.0f, gameOver.y - (gameOverTexture.height * gameOverScale)/2.0f }, 0.0f, gameOverScale, WHITE);
+    DrawTextEx(GetFontDefault(), "Game Over (nul)" , gameOver , 20.0f , 5.0f , BLACK);
 
     /* retry */
-    Texture2D retryTexture = LoadTexture("assets/menu/icons/retry.png");
     Vector2 retry          = { GetScreenWidth() * 0.45f, GetScreenHeight() * 0.6f };
-    float retryScale       = 0.15f;
-    DrawTextureEx(retryTexture, { retry.x - (retryTexture.width * retryScale)/2.0f, retry.y - (retryTexture.height * retryScale)/2.0f }, 0.0f, retryScale, WHITE);
+    DrawTextEx(GetFontDefault(), "Play" , retry , 20.0f , 5.0f , BLACK);
 
     /* levels */
     Texture2D levelsTexture = LoadTexture("assets/menu/icons/levels.png");
